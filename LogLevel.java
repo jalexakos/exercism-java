649b394aa95bdb037701188f86219dc1b6216a2c
Josh Alexakos <josiah.alexakos@gmail.com>
2024-11-08 15:49:15 -0800

    Solution for Log Level (Learning Java)

---
+++ LogLevel.java
@@ -0,0 +1,13 @@
+// from https://exercism.org/tracks/java/exercises/logs-logs-logs
+
+public enum LogLevel {
+    TRACE,
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    FATAL,
+    UNKNOWN
+
+    
+}
